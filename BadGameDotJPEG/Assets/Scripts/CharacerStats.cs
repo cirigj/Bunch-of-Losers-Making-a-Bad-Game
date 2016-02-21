a599c4802af713dbc0de69f502f9219a00c4b4d3
brolita <byrnec3@rpi.edu>
2016-02-20 20:19:27 -0500

    Base Input / Movement / Death

---
+++ BadGameDotJPEG/Assets/Scripts/CharacerStats.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacerStats : MonoBehaviour
+{
+    public int Lives;
+    public int Bombs;
+    private bool hit;
+    private int hitCounter;
+
+	void Start ()
+    {
+        Lives = Globals.BaseLives;
+        Bombs = Globals.BaseBombs;
+        hit = false;
+	}
+	
+	void Update ()
+    {
+	    
+	}
+
+    void Death()
+    {
+
+    }
+
+    public void StartDeathRoutine()
+    {
+        hit = true;
+        hitCounter = 0;
+    }
+
+    IEnumerator DeathRoutine()
+    {
+        hitCounter += 1;
+        if(Globals.Inputs.Bomb)
+        {
+            yield break;
+        }
+        if(hitCounter == 3)
+        {
+            Death();
+            yield break;
+        }
+        yield return new WaitForFixedUpdate();
+    }
+}
---
+++ BadGameDotJPEG/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public bool focus;
+    public float speed;
+    public float focusSpeed;
+    public float horizontal;
+    public float vertical;
+    public Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	void Update ()
+    {
+        if (focus)
+            rb.velocity = new Vector2(Globals.Inputs.Horizontal * focusSpeed, Globals.Inputs.Vertical * focusSpeed);
+        else
+            rb.velocity = new Vector2(Globals.Inputs.Horizontal * speed, Globals.Inputs.Vertical * speed);
+    }
+}
